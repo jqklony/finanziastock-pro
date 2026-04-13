a5de0838b63a14d1927e8adad327060a1f978d98
jqklony <jqklony@gmail.com>
2026-04-13 13:21:47 -0400

    fix: add @tailwindcss/postcss - CSS now renders correctly with all Tailwind classes

--- out/_next/static/chunks/app/layout-e66ca81b60dabeab.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},1076:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,347,23))}},e=>{e.O(0,[873,441,964,358],()=>e(e.s=1076)),_N_E=e.O()}]);
